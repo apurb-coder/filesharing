ab6e7fefccbd57c592e011da2cbca60725899a95
Apurba <55659698+apurb-coder@users.noreply.github.com>
2023-07-30 19:21:52 +0530

    Add files via upload

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -16,7 +16,11 @@ router.post('/upload' ,upload.single('file') ,async(req,res)=>{
     }
     try{
         const file= await File.create(fileObj);//saving file in the database
-        res.status(200).json({path: `${process.env.DOWNLOAD_URL_BACKEND_LINK}/files/${file._id}`})
+        res
+          .status(200)
+          .json({
+            path: `${process.env.DOWNLOAD_URL_BACKEND_LINK}/files/${file._id}`,
+          });
     }
     catch(error){
         console.log(error.message)
@@ -29,14 +33,14 @@ router.post('/upload' ,upload.single('file') ,async(req,res)=>{
 router.get('/files/:fileId',async(req,res)=>{
   // :fileId is a variable
   try {
-    //we are extracting the fileId portion from the url
-    const file = await File.findById(req.params.fileId);
+    //we are extracting the fileId portion from the url by using req.params.fileId
+    const file = await File.findById(req.params.fileId);//finding the file
     file.downloadContent++;
 
     await file.save();
 
     //below function sending download response
-    res.download(file.path, file.name); //download is a inbuilt function to perform download
+    res.download(file.path, file.name); //download() is a inbuilt function to perform download
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
